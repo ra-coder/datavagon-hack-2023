634bacc3125bc2b806265c2970184e1745fbeb92
Maksim Shtyr <sh.maxim97@gmail.com>
2023-11-11 15:34:22 +0300

    init frontend

---
+++ front/src/Train.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {Ymaps, withMap} from './withMap';
+import { LngLat } from '@yandex/ymaps3-types';
+
+type TrainProps = Ymaps & {
+    id: string;
+}
+
+interface Event {
+    moment: number
+    vagon_ids: number[];
+    dislocation: {
+        id: number;
+        name: string;
+        latitude: number;
+        longitude: number;
+    },
+}
+
+interface Timeline {
+    train: {
+        name: string;
+        train_index: string;
+    }
+    events: Event[]
+}
+
+function getLngLat(event: Event): LngLat {
+    return [event.dislocation.longitude, event.dislocation.latitude];
+}
+
+function IntTrain({id, ymaps}: TrainProps) {
+    const [timeline, setTimeline] = React.useState<Timeline>();
+
+    React.useEffect(() => {
+        const url = './train.json'; // `http://158.160.78.28:8000/api/v1/train/${id}/timeline`;
+        fetch(url).then((data) => data.json()).then((data) => {
+            console.log(data);
+            setTimeline(data);
+        })
+    }, []);
+
+    if (!timeline) return;
+
+    return (
+        <>
+            {timeline.events.map((event, index) => (
+                <ymaps.YMapMarker key={index} coordinates={getLngLat(event)} properties={{hint: new Date(event.moment * 1000).toString()}}>
+                    <div style={{width: 16, height: 16, background: '#7036BD', transform: 'translate(-50%, -50%)'}}>
+                        {// new Date(event.moment * 1000).toString()}
+                        }
+                    </div>
+                </ymaps.YMapMarker>
+            ))}
+            <ymaps.YMapFeature geometry={{type: 'LineString', coordinates: timeline.events.map(getLngLat)}} />
+            <ymaps.YMapHint hint={object => object?.properties?.hint}>
+                <TrainHint />
+            </ymaps.YMapHint>
+        </>
+    );
+}
+
+const TrainHint = withMap(function TrainHintInt({ymaps}: Ymaps) {
+    const ctx = React.useContext(ymaps.YMapHintContext);
+    return <div className="hint">{ctx && ctx.hint}</div>;
+})
+
+export const Train = withMap(IntTrain);
---
+++ front/src/withMap.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import type Main from '@yandex/ymaps3-types/react';
+import type SphericalMerkatorPackage from '@yandex/ymaps3-types/packages/spherical-mercator-projection';
+import type ControlsPackage from '@yandex/ymaps3-types/packages/controls/react';
+
+interface HintPackage {
+    YMapHint: React.FC<React.PropsWithChildren<
+        {hint: (object: {properties: {hint: any}} | undefined) => unknown}
+    >>;
+    YMapHintContext: React.Context<{hint: any}>;
+}
+
+type YmapsModule = typeof Main & typeof SphericalMerkatorPackage & typeof ControlsPackage & HintPackage;
+export interface Ymaps {
+    ymaps: YmapsModule
+}
+
+let ymapsPromise: Promise<YmapsModule>;
+export function withMap<T extends Ymaps>(Child: React.ComponentType<T>) {
+    return (props: Omit<T, 'ymaps'>) => {
+        const [ymaps, setYmaps] = React.useState<YmapsModule>();
+
+        React.useEffect(() => {
+            ymapsPromise ??= getYmapsPromise();
+
+            ymapsPromise.then(setYmaps)
+        }, [])
+
+        if (!ymaps) return;
+
+        return (
+            <Child {...props as T} ymaps={ymaps} />
+        );
+    }
+}
+
+async function getYmapsPromise(): Promise<YmapsModule> {
+    const [ymaps3React] = await Promise.all([ymaps3.import('@yandex/ymaps3-reactify'), ymaps3.ready]);
+    const reactify = ymaps3React.reactify.bindTo(React, ReactDOM);
+
+    const sphericalMercator = await ymaps3.import('@yandex/ymaps3-spherical-mercator-projection@0.0.1');
+    const controls = reactify.module(await ymaps3.import('@yandex/ymaps3-controls@0.0.1'));
+    const hint = reactify.module(await ymaps3.import('@yandex/ymaps3-hint@0.0.1'));
+
+    return {
+        ...reactify.module(ymaps3),
+        ...sphericalMercator,
+        ...controls,
+        ...hint as unknown as HintPackage,
+        reactify
+    };
+}
